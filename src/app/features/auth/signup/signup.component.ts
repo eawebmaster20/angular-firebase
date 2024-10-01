2207b7b8bc5b8964dd800cd44f8b96643164963b
eawebmaster20 <eaweb20@gmail.com>
2024-10-01 16:05:36 +0000

    feat: add signup logic and template to authenticate

--- src/app/features/auth/signup/signup.component.ts
+++ src/app/features/auth/signup/signup.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, updateProfile } from '@angular/fire/auth';
+import { FormsModule } from '@angular/forms';
+import { catchError, from, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
-
+username=''
+email=''
+password=''
+constructor(private firebaseAuth: Auth){
+ 
+}
+signup(){
+  console.log(this.username, this.email, this.password)
+  const promise = createUserWithEmailAndPassword(this.firebaseAuth, this.email, this.password)
+  .then((res:any) => updateProfile(res.user, {displayName: this.username}))
+  from(promise).pipe(
+    catchError(() => {throw new Error('dijos')})
+  ).subscribe({
+    next: (value) => {
+      console.log('Success', value);
+    },
+    error: (error) => {
+      console.error('Error', error);
+    }
+  })
+}
 }
