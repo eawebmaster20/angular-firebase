0866b4c356a420cb96407cf59d0d821fc4237ead
eawebmaster20 <eaweb20@gmail.com>
2024-10-01 16:02:50 +0000

    core: add routes for various endpoints

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'post-comment' },
+    { path: 'create-post', loadComponent: () => import('./features/post/create/create.component').then(m => m.CreateComponent) },
+    { path: 'post-details', loadComponent: () => import('./features/post/post-details/post-details.component').then(m => m.PostDetailsComponent) },
+    { path: 'post-comment', loadComponent: () => import('./features/post/comments/comments.component').then(m => m.CommentsComponent) },
+    { path: 'login', loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent) },
+    { path: 'register', loadComponent: () => import('./features/auth/signup/signup.component').then(m => m.SignupComponent) },
+    { path: '**', redirectTo: 'post-comment' }
+];
