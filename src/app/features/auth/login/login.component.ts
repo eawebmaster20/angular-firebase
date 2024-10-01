2f4671d58765ba89efb11d83beb0f5f8d437317c
eawebmaster20 <eaweb20@gmail.com>
2024-10-01 16:05:17 +0000

    feat: add login logic and template to authenticate

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { FormsModule } from '@angular/forms';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+email=''
+password=''
 
+constructor(private firebaseAuth: Auth){}
+login(){
+  signInWithEmailAndPassword(this.firebaseAuth,this.email, this.password)
+  .then((res:any)=>{console.log(res);})
+}
+logout(){
+  this.firebaseAuth.signOut()
+}
+
+googleAuth(){
+  signInWithPopup(this.firebaseAuth,new GoogleAuthProvider()).then((res:any)=>{console.log(res)})
+  }
 }
