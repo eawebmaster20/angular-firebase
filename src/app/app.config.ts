0fabd8bff31425652907b7cb8a5c8c02ccf0a040
eawebmaster20 <eaweb20@gmail.com>
2024-10-01 16:10:19 +0000

    core: add firebase configuration to app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,40 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app'
+import { getAuth, provideAuth } from '@angular/fire/auth'
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBuuxrL3nOk_KWqBowDrmM-foWDuJmJbaM",
+  authDomain: "analytics-crud.firebaseapp.com",
+  databaseURL: "https://analytics-crud-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "analytics-crud",
+  storageBucket: "analytics-crud.appspot.com",
+  messagingSenderId: "918538008426",
+  appId: "1:918538008426:web:5cc2edd073bfcc5defd713",
+  measurementId: "G-Q0MK4ZMRM5"
+};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBuuxrL3nOk_KWqBowDrmM-foWDuJmJbaM",
+//   authDomain: "analytics-crud.firebaseapp.com",
+//   projectId: "analytics-crud",
+//   storageBucket: "analytics-crud.appspot.com",
+//   messagingSenderId: "918538008426",
+//   appId: "1:918538008426:web:5cc2edd073bfcc5defd713",
+//   measurementId: "G-Q0MK4ZMRM5"
+// }
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(),
+    provideFirebaseApp(()=>initializeApp(firebaseConfig)),
+    provideAuth(()=> getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(()=> getFirestore())
+    // importProvidersFrom([
+    // ])
+  ]
 };
