7daddabcd0690e8707d5da9070c4d7620eb7214a
eawebmaster20 <eaweb20@gmail.com>
2024-10-02 19:16:37 +0000

    core: pwa integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app'
 import { getAuth, provideAuth } from '@angular/fire/auth'
@@ -6,6 +6,7 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideServiceWorker } from '@angular/service-worker';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBuuxrL3nOk_KWqBowDrmM-foWDuJmJbaM",
@@ -28,13 +29,18 @@ const firebaseConfig = {
 // }
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
+    provideRouter(routes),
     provideClientHydration(),
-    provideFirebaseApp(()=>initializeApp(firebaseConfig)),
-    provideAuth(()=> getAuth()),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
-    provideFirestore(()=> getFirestore())
+    provideFirestore(() => getFirestore())
     // importProvidersFrom([
     // ])
-  ]
+    ,
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
