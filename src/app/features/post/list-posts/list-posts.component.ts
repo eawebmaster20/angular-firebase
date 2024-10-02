c4dd9eff299d1a71454ef29f9bff3fe6dc25c58c
eawebmaster20 <eaweb20@gmail.com>
2024-10-02 19:11:25 +0000

    feat: update post list component logic and template

--- src/app/features/post/list-posts/list-posts.component.ts
+++ src/app/features/post/list-posts/list-posts.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IPost } from '../../../core/interfaces/posts.interface';
+import { PostService } from '../../../core/services/post/post.service';
 
 @Component({
   selector: 'app-list-posts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list-posts.component.html',
   styleUrl: './list-posts.component.scss'
 })
-export class ListPostsComponent {
+export class ListPostsComponent implements OnInit {
+  posts:any = [  ]
 
+  constructor(private postService:PostService) { }
+  ngOnInit(): void {
+    this.postService.getAllPosts().subscribe({
+      next: (snapshot) => {
+        snapshot.forEach(childSnapshot => {
+          this.posts.push({ id: childSnapshot.key, ...childSnapshot.val() });
+        });
+      },
+      error: error => console.error('Error getting posts:', error)
+    })
+  }
+  deletePost(id: string): void {
+    this.postService.deletePost(id).subscribe({
+      next: () => {
+        console.log('Post deleted successfully');
+      },
+      error: error => {
+        console.error('Error deleting post:', error);
+      }
+    })
+  }
 }
