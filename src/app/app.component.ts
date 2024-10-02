4fc7aaac6de88e3f91ce07f72dbceb64ad1d57a8
eawebmaster20 <eaweb20@gmail.com>
2024-10-02 16:28:19 +0000

    core: add buttons to test crud operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,60 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Auth, linkWithCredential, PhoneAuthProvider, RecaptchaVerifier, signInWithPhoneNumber, updateProfile, user } from '@angular/fire/auth';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { FirebaseApp, initializeApp } from '@angular/fire/app'; // Import FirebaseApp
+import { UserProfileComponent } from './features/user-profile/user-profile.component';
+import { ListPostsComponent } from './features/post/list-posts/list-posts.component';
+import { TextEditorComponent } from './features/CVA/text-editor/text-editor.component';
+import { FormsModule } from '@angular/forms';
+import { PostService } from './core/services/post/post.service';
+import { IResponseUser } from './core/interfaces/user.interface';
+import { ProfileService } from './core/services/profile/profile.service';
+import { AuthService } from './core/services/auth/auth.service';
 
+type User = import('firebase/auth').User;
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TextEditorComponent, FormsModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit, AfterViewInit {
   user$ = user(this.firebaseAuth)
   recaptchaVerifier!: RecaptchaVerifier;
-  title = 'firebase-crud';
-
-  constructor(private firebaseAuth: Auth, private firebaseApp: FirebaseApp) {} // Inject FirebaseApp
+  texteditor = ''
+  constructor(
+    private firebaseAuth: Auth, 
+    private authService: AuthService, 
+    public postService:PostService,
+    private profileService: ProfileService,
+  ) {} 
 
   ngOnInit(): void {
     this.user$.subscribe({
-      next: user => {
+      next: (user: User|null) => {
         console.log(user);
-        // if (user) {
-        //   this.updateUserProfile(
-        //     user, 
-        //     { 
-        //       displayName: 'newly set name', 
-        //       photoURL:'https://www.yttags.com/blog/image-url-for-testing/'
-        //     });
-        // } else {
-        //   console.log('No user is signed in');
-        // }
+          this.profileService.user = user || null;
       },
       error: error => {},
       complete: () => {}
     })
   }
-
+  log(){
+    console.log(this.texteditor)
+  }
   ngAfterViewInit(): void {
     // this.setupPhoneVerification();
   }
-  updateUserProfile(user: any, profileData: { displayName?: string; photoURL?: string }): void {
+
+  logout(){
+    this.authService.logout().subscribe({
+      next: (value) => {console.log(value)},
+      error: (error) => {console.error(error)}
+    });
+  }
+  updateUserProfile(user: User, profileData: { displayName?: string; photoURL?: string }): void {
+    console.log(user)
     updateProfile(user, profileData).then(() => {
       console.log('User profile updated successfully');
     }).catch(error => {
