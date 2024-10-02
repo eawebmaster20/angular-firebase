34a0814e9c7720307c7c7fe2c4ff1ee130c56a29
eawebmaster20 <eaweb20@gmail.com>
2024-10-02 19:12:06 +0000

    feat: update post creating component's logic and template

--- src/app/features/post/create/create.component.ts
+++ src/app/features/post/create/create.component.ts
@@ -2,6 +2,8 @@ import { AfterViewInit, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Database, ref, set, push, child, get, update, remove } from '@angular/fire/database';
 import { Observable } from 'rxjs';
+import { IPost } from '../../../core/interfaces/posts.interface';
+import { PostService } from '../../../core/services/post/post.service';
 
 @Component({
   selector: 'app-create',
@@ -17,37 +19,25 @@ import { Observable } from 'rxjs';
   update a post in the realtime post Collection,
   and delete a post in the realtime post Collection.
   */
-export class CreateComponent implements AfterViewInit{
-  title = '';
-  body = '';
-
-  post: any = {
+export class CreateComponent{
+  post: IPost = {
     title: '',
-    body: ''
+    body: '',
+    userId: ''
   };
 
-  constructor(private newDb: Database) {}
-  ngAfterViewInit(): void {
-    this.getAllPosts().subscribe({
-      next:( posts:any) => {
-        console.log(posts)
+  constructor(private newDb: Database, private postService:PostService) {}
+  createPost() {
+    this.postService.createPost(this.post).subscribe({
+      next: () => {
+        console.log('Post created successfully');
       },
-      error: error => console.error('Error getting posts:', error)
+      error: (error) => {
+        console.error('Error creating post:', error);
+      }
     })
   }
 
-  
-  createPost() {
-    const postsRef = ref(this.newDb, 'posts');
-    const newPostRef = push(postsRef);
-    set(newPostRef, this.post)
-      .then(() => {
-        console.log('Post created successfully:', this.post);
-      })
-      .catch(error => {
-        console.error('Error creating post:', error);
-      });
-  }
 
  
   getSinglePost(postId: string) {
@@ -60,58 +50,57 @@ export class CreateComponent implements AfterViewInit{
           console.log('No post found');
           }
         })
-   
   }
 
 
-  getAllPosts() {
-    const postsRef = ref(this.newDb, 'posts');
-    return new Observable(observer => {
-      get(postsRef)
-        .then(snapshot => {
-          const posts:any = [];
-          snapshot.forEach(childSnapshot => {
-            posts.push({ id: childSnapshot.key, ...childSnapshot.val() });
-          });
-          observer.next(posts);
-        })
-        .catch(error => observer.error(error));
-    });
-  }
+  // getAllPosts() {
+  //   const postsRef = ref(this.newDb, 'posts');
+  //   return new Observable(observer => {
+  //     get(postsRef)
+  //       .then(snapshot => {
+  //         const posts:IPost[] = [];
+  //         snapshot.forEach(childSnapshot => {
+  //           posts.push({ id: childSnapshot.key, ...childSnapshot.val() });
+  //         });
+  //         observer.next(posts);
+  //       })
+  //       .catch(error => observer.error(error));
+  //   });
+  // }
 
-  addPost() {
-    const postsRef = ref(this.newDb, 'posts');
-    const newPostRef = push(postsRef);
-    set(newPostRef, { title: this.title, body: this.body })
-      .then(() => {
-        console.log('Post added successfully');
-      })
-      .catch(error => {
-        console.error('Error adding post:', error);
-      });
-  }
-
-
-  deletePost(postId: string) {
-    const postRef = ref(this.newDb, `posts/${postId}`);
-    remove(postRef)
-      .then(() => {
-        console.log('Post deleted successfully');
-      })
-      .catch(error => {
-        console.error('Error deleting post:', error);
-      });
-  }
+  // addPost() {
+  //   const postsRef = ref(this.newDb, 'posts');
+  //   const newPostRef = push(postsRef);
+  //   set(newPostRef, { title: this.title, body: this.body })
+  //     .then(() => {
+  //       console.log('Post added successfully');
+  //     })
+  //     .catch(error => {
+  //       console.error('Error adding post:', error);
+  //     });
+  // }
 
 
-  updatePost(postId: string, title: string, body: string) {
-    const postRef = ref(this.newDb, `posts/${postId}`);
-    update(postRef, { title, body })
-      .then(() => {
-        console.log('Post updated successfully');
-      })
-      .catch(error => {
-        console.error('Error updating post:', error);
-      });
-  }
+  // deletePost(postId: string) {
+  //   const postRef = ref(this.newDb, `posts/${postId}`);
+  //   remove(postRef)
+  //     .then(() => {
+  //       console.log('Post deleted successfully');
+  //     })
+  //     .catch(error => {
+  //       console.error('Error deleting post:', error);
+  //     });
+  // }
+
+
+  // updatePost(postId: string, title: string, body: string) {
+  //   const postRef = ref(this.newDb, `posts/${postId}`);
+  //   update(postRef, { title, body })
+  //     .then(() => {
+  //       console.log('Post updated successfully');
+  //     })
+  //     .catch(error => {
+  //       console.error('Error updating post:', error);
+  //     });
+  // }
 }
