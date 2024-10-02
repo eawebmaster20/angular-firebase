ad351ef7b3148fe2585a18a8f8a07a97c08ba497
eawebmaster20 <eaweb20@gmail.com>
2024-10-02 19:13:33 +0000

    feat: update post detail component's logic

--- src/app/features/post/post-details/post-details.component.ts
+++ src/app/features/post/post-details/post-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { PostService } from '../../../core/services/post/post.service';
+import { IPost } from '../../../core/interfaces/posts.interface';
 
 @Component({
   selector: 'app-post-details',
@@ -7,6 +10,30 @@ import { Component } from '@angular/core';
   templateUrl: './post-details.component.html',
   styleUrl: './post-details.component.scss'
 })
-export class PostDetailsComponent {
+export class PostDetailsComponent implements OnInit {
 
+  constructor(private metaService: Meta, private titleService: Title, private postService:PostService){}
+
+  ngOnInit(): void {
+    // Set title and meta tags when the component is initialized
+    this.updateMetaTags();
+    this.postService.getSinglePost('-O8CVQluvYeLhSFwI1nj').subscribe({
+      next: (snapshot) => {
+        console.log({ id: snapshot.key, ...snapshot.val() });
+      },
+      error: error => console.error('Error getting post:', error)
+    })
+  }
+
+  updateMetaTags() {
+    this.titleService.setTitle('My Sample Page Title');
+
+    // Add or update meta tags
+    this.metaService.updateTag({ name: 'description', content: 'This is a sample description for SEO purposes.' });
+    this.metaService.updateTag({ name: 'keywords', content: 'Angular, SEO, MetaService' });
+
+    // You can also add or remove tags dynamically
+    this.metaService.addTag({ name: 'author', content: 'Your Name' });
+    this.metaService.removeTag('name="keywords"');  // Example: remove the 'keywords' tag
+  }
 }
