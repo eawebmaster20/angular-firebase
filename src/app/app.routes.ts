ff84c48cc0053c8de5a481c277c64be08065a2f8
eawebmaster20 <eaweb20@gmail.com>
2024-10-03 07:32:00 +0000

    core: routes updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    // { path: '', pathMatch: 'full', redirectTo: 'post-comment' },
-    { path: '', loadComponent: () => import('./app.component').then(m => m.AppComponent) },
+    { path: '', pathMatch: 'full', redirectTo: 'post-list' },
+    // { path: '', loadComponent: () => import('./app.component').then(m => m.AppComponent) },
     { path: 'create-post', loadComponent: () => import('./features/post/create/create.component').then(m => m.CreateComponent) },
     { path: 'post-details', loadComponent: () => import('./features/post/post-details/post-details.component').then(m => m.PostDetailsComponent) },
     { path: 'post-list', loadComponent: () => import('./features/post/list-posts/list-posts.component').then(m => m.ListPostsComponent) },
@@ -10,5 +10,5 @@ export const routes: Routes = [
     { path: 'profile', loadComponent: () => import('./features/user-profile/user-profile.component').then(m => m.UserProfileComponent) },
     { path: 'login', loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent) },
     { path: 'register', loadComponent: () => import('./features/auth/signup/signup.component').then(m => m.SignupComponent) },
-    { path: '**', redirectTo: 'post-comment' }
+    { path: '**', redirectTo: 'post-list' }
 ];
