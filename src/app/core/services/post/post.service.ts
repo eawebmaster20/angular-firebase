1a07d50faa8866f6d6bf14b7c2d20f160565ccc2
eawebmaster20 <eaweb20@gmail.com>
2024-10-04 06:55:23 +0000

    core: add service to manage localstorage

--- src/app/core/services/post/post.service.ts
+++ src/app/core/services/post/post.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Database, ref, set, push, child, get, update, remove } from '@angular/fire/database';
 import { IPost } from '../../interfaces/posts.interface';
 import { from, Observable } from 'rxjs';
+import { addDoc, collection, collectionData, doc, Firestore } from '@angular/fire/firestore';
+import { IComment } from '../../interfaces/comment.interface';
+import { deleteDoc } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +17,10 @@ export class PostService {
     userId: 'Author',
     category: 'Development'
   }
-  constructor(private newDb: Database) { }
+  constructor(
+    private newDb: Database,
+    private firestore: Firestore
+  ) { }
 
   createPost(post: IPost){
     const postsRef = ref(this.newDb, 'posts');
@@ -46,5 +52,20 @@ export class PostService {
   updatePost(postId: string, post:Partial<IPost>) {
     const postRef = ref(this.newDb, `posts/${postId}`);
     update(postRef, post)
+  }
+
+  createComment(comment:IComment){
+    const commentCollection = collection(this.firestore, 'comments');
+    return from(addDoc(commentCollection, comment))
+  }
+  
+  getAllPostComments(postId:string): Observable<IComment[]> {
+    const commentsCollection = collection(this.firestore, 'comments');
+    return collectionData(commentsCollection, { idField: 'id' }) as Observable<IComment[]>;
+  }
+
+  deleteComment(commentId: string) {
+    const commentDocRef = doc(this.firestore, `comments/${commentId}`);
+    return from(deleteDoc(commentDocRef));
   }
 }
