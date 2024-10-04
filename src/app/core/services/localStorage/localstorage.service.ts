ae46ba3424b7711a6d8f292c655e76b2a6b74fbe
eawebmaster20 <eaweb20@gmail.com>
2024-10-04 09:47:07 +0000

    core: firebase update

--- src/app/core/services/localStorage/localstorage.service.ts
+++ src/app/core/services/localStorage/localstorage.service.ts
@@ -1,21 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalstorageService {
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
   setItem(key: string, value: any): void {
-    localStorage.setItem(key, JSON.stringify(value));
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
   }
 
   getItem(key: string): any {
-    const data = localStorage.getItem(key);
-    return data ? JSON.parse(data) : null;
+    if (isPlatformBrowser(this.platformId)) {
+      const data = localStorage.getItem(key);
+      return data ? JSON.parse(data) : null;
+    }
+    return null; // Fallback in case localStorage is unavailable
   }
 
   removeItem(key: string): void {
-    localStorage.removeItem(key);
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem(key);
+    }
   }
 }
